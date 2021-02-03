26b252735314b00300157f94e2e755cef0bbd0f0
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-02-02 18:52:49 -0600

    Add project files.

---
+++ MIS 3013/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MIS_3013_VariablesInputConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+            double gpa;
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; // idk why but we want to bump everyones gpa by .5
+
+            string output = "Hello " + firstName + " " + lastName + "! " + 
+                             " we boosted your gpa to " + gpa.ToString("N3");
+            //output = $"Hello {firstName} {lastName}! we boosted your gpa to {gpa.ToString("N3")}
+            
+            Console.WriteLine(output);
+
+        }
+    }
+}
